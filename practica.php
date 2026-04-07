eeab409dfcc73727b0ebd9a35d10a508a638c28d
Carlos Díaz <cedg_23@outlook.com>
2026-04-07 11:25:45 -0500

    Create practica.php
    Se crea practica.php

---
+++ practica.php
@@ -0,0 +1,25 @@
+<?php
+//1. suma (+)
+$numero1 = 5;
+$numero2 = 3;
+$resultado = $numero1 + $numero2;
+echo "La suma de: " . $numero1 . " + " . $numero2 . " es igual a: " . $resultado ."<br>";
+
+//2. resta (-)
+$numero1 = 10;
+$numero2 = 4;
+$resultado = $numero1 - $numero2;
+echo "La resta de: " . $numero1 . " - " . $numero2 . " es igual a: " . $resultado ."<br>";
+
+//3. multiplicación (*)
+$numero1 = 7;
+$numero2 = 3;
+$resultado = $numero1 * $numero2;
+echo "La multiplicación de: " . $numero1 . " X " . $numero2 . " es igual a: " . $resultado ."<br>";
+
+$resultado = 2 + 3 * 4;
+echo "2 + 3 * 4 = " . $resultado ."<br>";
+
+$resultado = (2 + 3) * 4;
+echo "(2 + 3) * 4 = " . $resultado ."<br>";
+?>
